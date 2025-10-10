4361ac215fe160b10001d18521e4fde9bff299a6
Roman <neznayuhto@gmail.com>
2025-10-10 17:25:30 +0300

    Изменение сериалайзеров в приложении materials

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,15 +1,23 @@
 from rest_framework.serializers import ModelSerializer
-
+from rest_framework import serializers
 from materials.models import Course, Lesson
-
-
-class CourseSerializer(ModelSerializer):
-    class Meta:
-        model = Course
-        fields = "__all__"
 
 
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+
+class CourseSerializer(ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    def get_lessons_count(self, course):
+        return course.lessons.count()
+
+    class Meta:
+        model = Course
+        fields = ("id", "name", "description", "lessons_count", "lessons")
+
+
+
