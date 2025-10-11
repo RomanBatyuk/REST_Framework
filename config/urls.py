2876aee2d5f5831cd51963bfdedee97e127441b0
Roman <neznayuhto@gmail.com>
2025-10-11 14:17:03 +0300

    Обновление настроек и путей проекта

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("course/", include("materials.urls", namespace="course")),
-    path('users/', include('users.urls')),
+    path('users/', include('users.urls', namespace="users")),
 ]
