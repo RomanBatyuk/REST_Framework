e58ffa3f50c5ed937a2a92230640c96cdf559844
Roman <neznayuhto@gmail.com>
2025-10-11 14:14:08 +0300

    Добавление JWT-авторизации в приложение users

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from rest_framework.routers import SimpleRouter
 from users.views import PaymentViewSet
 from users.apps import UsersConfig
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
 app_name = UsersConfig.name
 
 router = SimpleRouter()
 router.register("payments", PaymentViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+] + router.urls
