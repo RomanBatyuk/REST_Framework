79d01956117b9fb5221c9b870410fcb999d72ef9
Roman <neznayuhto@gmail.com>
2025-10-16 21:50:26 +0300

    Добавление модели подписки в приложении materials

--- materials/models.py
+++ materials/models.py
@@ -5,7 +5,7 @@ class Course(models.Model):
     name = models.CharField(max_length=50, verbose_name="Название курса", help_text="Введите название курса")
     image = models.ImageField(upload_to="users/image", blank=True, null=True, verbose_name="Картинка", help_text="Загрузите картинку")
     description = models.TextField(blank=True, null=True, verbose_name="Описание", help_text="Введите описание")
-    owner = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name="Владелец")
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE, verbose_name="Владелец")
 
     class Meta:
         verbose_name = "Курс"
@@ -19,8 +19,16 @@ class Lesson(models.Model):
     description = models.TextField(blank=True, null=True, verbose_name="Описание", help_text="Введите описание урока")
     video_url = models.URLField(max_length=500, blank=True, null=True, verbose_name="Ссылка на урок", help_text="Введите ссылку на видео")
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons', verbose_name="Курс")
-    owner = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name="Владелец")
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE, verbose_name="Владелец")
 
     class Meta:
         verbose_name = "Урок"
         verbose_name_plural = "Уроки"
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, verbose_name="Пользователь")
+    course = models.ForeignKey("materials.Course", on_delete=models.CASCADE, verbose_name="Курс")
+
+    class Meta:
+        unique_together = ('user', 'course')
