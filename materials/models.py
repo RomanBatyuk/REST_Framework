0424c9c589ddee0e70f0ee1b3942c5960b6eb34e
Roman <neznayuhto@gmail.com>
2025-10-21 22:39:07 +0300

    Добавление модели Course_purchase в приложение materials

--- materials/models.py
+++ materials/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 
 class Course(models.Model):
     name = models.CharField(max_length=50, verbose_name="Название курса", help_text="Введите название курса")
@@ -32,3 +34,38 @@ class Subscription(models.Model):
 
     class Meta:
         unique_together = ('user', 'course')
+
+
+class Course_purchase(models.Model):
+    amount = models.PositiveIntegerField(
+        verbose_name="Стоимость курса",
+        help_text="Укажите стоимость курса",
+    )
+    session_id = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        verbose_name="Id сессии",
+        help_text="Укажите Id сессии",
+    )
+    link = models.URLField(
+        max_length=1400,
+        blank=True,
+        null=True,
+        verbose_name="Ссылка на оплату",
+        help_text="Укажите ссылку на оплату",
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name="Пользователь",
+        help_text="Укажите пользователя",
+    )
+    class Meta:
+        verbose_name = "Купленный курс"
+        verbose_name_plural = "Купленные курсы"
+
+    def __str__(self):
+        return self.amount
