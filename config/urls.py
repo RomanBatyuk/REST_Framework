dd2c8f40957a896a1ad11f768c8f89cb44d15b7a
Roman <neznayuhto@gmail.com>
2025-10-21 22:35:36 +0300

    Добавление главных путей проекта

--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("course/", include("materials.urls", namespace="course")),
     path('users/', include('users.urls', namespace="users")),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
 ]
